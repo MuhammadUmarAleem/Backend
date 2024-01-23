991f6861f2ec6e2b3389d0591346349ecee11756
MuhammadUmarAleem <muhammadumaraleem@gmsil.com>
2024-01-23 22:39:29 +0500

    Commit

--- controller/GetRelams.js
+++ controller/GetRelams.js
@@ -2,7 +2,10 @@ const { connection } = require("../utils/database");
 async function GetRealms(req, response) {
   try {
     connection.query(
-      `SELECT realms.id,realms.name from realms JOIN companies on realms.company_id=companies.id`,
+      `SELECT realms.*
+      FROM realms
+      JOIN companies ON realms.company_id = companies.id;
+      `,
       (err, res) => {
         if (err) {
           return;
